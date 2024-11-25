ade4f7e6c48b92c867492b7aac7d66b9c380e82d
Raul <valls.raul@gmail.com>
2024-11-25 23:31:33 +0100

    Add project files.

---
+++ WebApiTesting/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiTesting.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "I'm alive!";
+        }
+
+        [HttpGet("HelloWorld", Name = "HelloWorld")]
+        public string GetHelloWorld()
+        {
+            return "Hello World";
+        }
+    }
+}
